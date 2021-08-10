e6b9d9593833cfe2abb2149f5fbda16250e20cc5
pal szentgyorgyi <pszentg@gmail.com>
2021-08-10 23:01:51 +0200

    replaced sqlite with mongodb

--- server.py
+++ server.py
@@ -1,19 +1,18 @@
-import math
-import sqlite3 as sql
-from collections import defaultdict
-
-from flask import Flask, request, jsonify
 import json
+import logging
+import math
+from collections import defaultdict
+from flask_pymongo import PyMongo
+from flask import Flask, request, jsonify
+from numpy import mean
+
+logger = logging.getLogger()
 
 app = Flask(__name__)
-# create tables if they don't exist
-with sql.connect('database.db') as con:
-    try:
-        con.execute('CREATE TABLE ints (value INT)')
-        con.execute('CREATE TABLE floats (value FLOAT)')
-        con.execute('CREATE TABLE strings (value TEXT)')
-    except sql.OperationalError:
-        print('tables already exist')
+app.config["MONGO_URI"] = \
+    "mongodb+srv://user:HcSrNasKdaBBwJ0z@cluster0.wyb5l.mongodb.net/minimal_webapi?retryWrites=true&w=majority"
+mongo = PyMongo(app)
+db = mongo.db
 
 # create a ledger that locally tracks the values
 ledger = {"strings": defaultdict(int),
@@ -25,118 +24,94 @@ ledger = {"strings": defaultdict(int),
 # still would be possible if I sent a body with the GET request, but not a good practice
 @app.route('/', methods=['POST'])
 def receive():
-    with sql.connect('database.db') as conn:
-        cur = conn.cursor()
-        conn.row_factory = sql.Row
-        try:
-            content = request.get_json()
-            data = json.loads(content)
-            payload = data['payload']
+    content = request.get_json()
+    data = json.loads(content)
+    payload = data['payload']
+    entry = {'value': payload}
+    # insert into corresponding table depending on the type of value
+    if type(payload) is int:
+        ledger['ints'][payload] += 1
+        db.ints.insert_one(entry)
+    elif type(payload) is float:
+        ledger['floats'][payload] += 1
+        db.floats.insert_one(entry)
+    elif type(payload) is str:
+        ledger['strings'][payload] += 1
+        db.strings.insert_one(entry)
+    else:
+        raise TypeError("Unknown data type")
 
-            # insert into corresponding table depending on the type of value
-            if type(payload) is int:
-                cur.execute("INSERT INTO ints (value) VALUES(?)", (payload,))
-                ledger['ints'][payload] += 1
-
-            elif type(payload) is float:
-                cur.execute("INSERT INTO floats (value) VALUES(?)", (payload,))
-                ledger['floats'][payload] += 1
-
-            elif type(payload) is str:
-                cur.execute("INSERT INTO strings (value) VALUES(?)", (payload,))
-                ledger['strings'][payload] += 1
-
-            else:
-                raise TypeError("Unknown data type")
-
-            return jsonify(success=True)
-
-        except Exception as e:
-            conn.rollback()
-            print(e)
-            return jsonify(success=False), 502
+    return jsonify(success=True)
 
 
 @app.route('/query_count', methods=["POST"])
 def query_count():
-    with sql.connect('database.db') as conn:
-        # cur = conn.cursor()
-        # conn.row_factory = sql.Row
-        try:
-            content = request.get_json()
-            data = json.loads(content)
-            payload = data['payload']
-            # because of the defaultdicts I don't have to worry about getting a KeyError here
-            if type(payload) is int:
-                print(f'{payload} was submitted {ledger["ints"][payload]} times in this session')
-                # this currently does not work as expected
-                # total = cur.execute(f"SELECT * FROM ints WHERE value = {payload}").rowcount
-                return jsonify({'payload': ledger["ints"][payload]}), 200
+    content = request.get_json()
+    data = json.loads(content)
+    payload = data['payload']
+    # because of the defaultdicts I don't have to worry about getting a KeyError here
+    if type(payload) is int:
+        print(f'{payload} was submitted {ledger["ints"][payload]} times in this session.')
+        collection = db.ints.find()
+        ints = [item for item in collection]
+        query = [item for item in ints if item['value'] == payload]
+        print(f'{payload} was submitted {len(query)} times since the DB was initialized last.')
 
-            if type(payload) is float:
-                print(f'{payload} was submitted {ledger["floats"][payload]} times in this session')
-                # total = cur.execute(f"SELECT * FROM floats WHERE value = {payload}").rowcount
-                return jsonify({'payload': ledger["ints"][payload]}), 200
+        # return jsonify({'payload': ledger["ints"][payload]}), 200
+        return jsonify({'payload': len(query)}), 200
 
-            if type(payload) is str:
-                print(f'{payload} was submitted {ledger["strings"][payload]} times in this session')
-                # total = cur.execute(f"SELECT * FROM strings WHERE value = {payload}").rowcount
-                return jsonify({'payload': ledger["strings"][payload]}), 200
+    if type(payload) is float:
+        print(f'{payload} was submitted {ledger["floats"][payload]} times in this session')
+        collection = db.floats.find()
+        floats = [item for item in collection]
+        query = [item for item in floats if item['value'] == payload]
+        print(f'{payload} was submitted {len(query)} times since the DB was initialized last.')
 
-        except Exception as e:
-            conn.rollback()
-            print(e)
-            return jsonify(success=False), 404
+        # return jsonify({'payload': ledger["ints"][payload]}), 200
+        return jsonify({'payload': len(query)}), 200
+
+    if type(payload) is str:
+        print(f'{payload} was submitted {ledger["strings"][payload]} times in this session')
+        collection = db.strings.find()
+        ints = [item for item in collection]
+        query = [item for item in ints if item['value'] == payload]
+        print(f'{payload} was submitted {len(query)} times since the DB was initialized last.')
+
+        # return jsonify({'payload': ledger["ints"][payload]}), 200
+        return jsonify({'payload': len(query)}), 200
 
 
 @app.route('/query_avg', methods=["POST"])
 def query_avg():
-    with sql.connect('database.db') as conn:
-        cur = conn.cursor()
-        conn.row_factory = sql.Row
-        try:
-            content = request.get_json()
-            data = json.loads(content)
-            payload = data['payload']
-            if payload == 'int':
-                # calculate avg of recorded ints in the db
-                total = 0
-                cur.execute("SELECT * FROM ints")
-                for row in cur.fetchall():
-                    total += row[0]
-                cur.execute("SELECT Count() FROM ints")
-                number_of_rows = cur.fetchone()[0]
-                avg = math.floor(total/number_of_rows)
+    content = request.get_json()
+    data = json.loads(content)
+    payload = data['payload']
+    print(f'payload is :{payload}')
+    total = 0
+    if payload == 'int':
+        collection = db.ints.find()
+        ints = [item['value'] for item in collection]
+        for k, v in ledger['ints'].items():
+            total += int(k) * int(v)
+        print(f'avg of ints: {math.floor(total / len(ledger["ints"]))}')
 
-                # calculate the avg in the ledger
-                total_nodb = 0
-                for k, v in ledger['ints'].items():
-                    total_nodb += int(k)*int(v)
-                print(f'avg of ints: {math.floor(total_nodb/len(ledger["ints"]))}')
+        # using numpy.mean() in case the db gets too large for statistics.mean()
+        return jsonify({'payload': mean(ints)}), 200
+        # return jsonify({'payload': mean(ledger["ints"])}), 200
 
-                return jsonify({'payload': avg}), 200
+    if payload == 'float':
+        collection = db.floats.find()
+        floats = [item['value'] for item in collection]
 
-            if payload == 'float':
-                total = 0.0
-                cur.execute("SELECT * FROM floats")
-                for row in cur.fetchall():
-                    total += row[0]
-                cur.execute("SELECT Count() FROM ints")
-                number_of_rows = cur.fetchone()[0]
-                avg = total / number_of_rows
+        for k, v in ledger['floats'].items():
+            total += float(k) * float(v)
+        print(f'avg of floats: {total / len(ledger["floats"].items())}')
 
-                total_nodb = 0
-                for k, v in ledger['floats'].items():
-                    total_nodb += float(k)*float(v)
-                print(f'avg of floats: {total_nodb/len(ledger["floats"].items())}')
-
-                return jsonify({'payload': avg}), 200
-
-        except Exception as e:
-            conn.rollback()
-            print(e)
-            return jsonify(success=False), 404
+        # using numpy.mean() in case the db gets too large for statistics.mean()
+        return jsonify({'payload': mean(floats)}), 200
+        # return jsonify({'payload': mean(ledger["ints"])}), 200
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
