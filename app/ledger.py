0926773608914755ec82dd82891a835f3d1d3852
pal szentgyorgyi <pszentg@gmail.com>
2021-08-11 21:14:43 +0200

    added argparse so the app can be switched to use a local ledger. default is  still db

--- app/ledger.py
+++ app/ledger.py
@@ -16,7 +16,7 @@ class Ledger:
         elif type(item) is str:
             return self.strings[item]
         else:
-            raise TypeError("Type is not tracked by the ledger.")
+            raise TypeError(f"Type is not tracked by the ledger:{item}.")
 
     def get_avg(self, d_type):
         dividend = 0
@@ -27,13 +27,12 @@ class Ledger:
                 dividend += int(k) * int(v)
                 divisor += v
 
-        if d_type == 'floats':
+        elif d_type == 'floats':
             for k, v in self.ints.items():
                 dividend += float(k) * float(v)
                 divisor += v
-
         else:
-            raise TypeError("Type is not tracked by the ledger.")
+            raise TypeError(f"Type is not tracked by the ledger:{d_type}.")
         try:
             return dividend / divisor
         except ZeroDivisionError:
--- app/server.py
+++ app/server.py
@@ -3,8 +3,12 @@ import logging
 from flask_pymongo import PyMongo
 from flask import Flask, request, jsonify
 from numpy import mean
+import argparse
 
-from ledger import Ledger
+parser = argparse.ArgumentParser(description='Process some incoming HTTP requests.')
+parser.add_argument('--ledger', '-l', help='Switches the server to store the data locally instead of a remote DB',
+                    action='store_true')
+args = parser.parse_args()
 
 logger = logging.getLogger()
 
@@ -14,89 +18,96 @@ app.config["MONGO_URI"] = \
 mongo = PyMongo(app)
 db = mongo.db
 
-# create a ledger that locally tracks the values
-ledger = Ledger()
+if args.ledger:
+    print('starting server with ledger instead of DB')
+    from ledger import Ledger
+    ledger = Ledger()
 
 
-# if i tracked only 1 type, eg. int, I'd only keep this route, and allow GET for queries.
-# still would be possible if I sent a body with the GET request, but not a good practice
 @app.route('/', methods=['POST'])
 def receive():
     content = request.get_json()
     data = json.loads(content)
     payload = data['payload']
-    entry = {'value': payload}
-    # insert into corresponding table/ledger depending on the type of value
-    if type(payload) is int:
-        ledger.ints[payload] += 1
-        db.ints.insert_one(entry)
-    elif type(payload) is float:
-        ledger.floats[payload] += 1
-        db.floats.insert_one(entry)
-    elif type(payload) is str:
-        ledger.strings[payload] += 1
-        db.strings.insert_one(entry)
+    if args.ledger:
+        if type(payload) is int:
+            ledger.ints[payload] += 1
+        elif type(payload) is float:
+            ledger.floats[payload] += 1
+        elif type(payload) is str:
+            ledger.strings[payload] += 1
+        else:
+            return jsonify('Unknown data type', success=False), 501
+
     else:
-        raise TypeError("Unknown data type")
+        entry = {'value': payload}
+        # insert into corresponding table depending on the type of value
+        if type(payload) is int:
+            db.ints.insert_one(entry)
+        elif type(payload) is float:
+            db.floats.insert_one(entry)
+        elif type(payload) is str:
+            db.strings.insert_one(entry)
+        else:
+            return jsonify('Unknown data type', success=False), 501
 
     return jsonify(success=True)
 
 
-@app.route('/query_count', methods=["POST"])
+@app.route('/count', methods=["POST"])
 def query_count():
     content = request.get_json()
     data = json.loads(content)
     payload = data['payload']
-    print(f'{payload} was submitted {ledger.get_count(payload)} according to the ledger')
-    # because of the defaultdicts I don't have to worry about getting a KeyError here
-    if type(payload) is int:
-        collection = db.ints.find()
-        ints = [item for item in collection]
-        query = [item for item in ints if item['value'] == payload]
-        print(f'{payload} was submitted {len(query)} times since the DB was initialized last.')
+    if args.ledger:
+        print(f'{payload} was submitted {ledger.get_count(payload)} according to the ledger')
+        return jsonify({'payload': ledger.get_count(payload)}), 200
 
-        # return jsonify({'payload': ledger.get_count(payload)}), 200
-        return jsonify({'payload': len(query)}), 200
+    else:
+        if type(payload) is int:
+            collection = db.ints.find()
+            query = [item for item in collection if item['value'] == payload]
+            print(f'{payload} was submitted {len(query)} times since the DB was initialized last.')
+            return jsonify({'payload': len(query)}), 200
 
-    if type(payload) is float:
-        collection = db.floats.find()
-        floats = [item for item in collection]
-        query = [item for item in floats if item['value'] == payload]
-        print(f'{payload} was submitted {len(query)} times since the DB was initialized last.')
+        if type(payload) is float:
+            collection = db.floats.find()
+            query = [item for item in collection if item['value'] == payload]
+            print(f'{payload} was submitted {len(query)} times since the DB was initialized last.')
+            return jsonify({'payload': len(query)}), 200
 
-        # return jsonify({'payload': ledger.get_count(payload)}), 200
-        return jsonify({'payload': len(query)}), 200
-
-    if type(payload) is str:
-        collection = db.strings.find()
-        ints = [item for item in collection]
-        query = [item for item in ints if item['value'] == payload]
-        print(f'{payload} was submitted {len(query)} times since the DB was initialized last.')
-
-        # return jsonify({'payload': ledger.get_count(payload)}), 200
-        return jsonify({'payload': len(query)}), 200
+        if type(payload) is str:
+            collection = db.strings.find()
+            query = [item for item in collection if item['value'] == payload]
+            print(f'{payload} was submitted {len(query)} times since the DB was initialized last.')
+            return jsonify({'payload': len(query)}), 200
 
 
-@app.route('/query_avg', methods=["POST"])
+@app.route('/avg', methods=["POST"])
 def query_avg():
     content = request.get_json()
     data = json.loads(content)
     payload = data['payload']
-    if payload == 'int':
-        collection = db.ints.find()
-        ints = [item['value'] for item in collection]
+    if args.ledger:
+        if payload == 'int':
+            return jsonify({'payload': ledger.get_avg('ints')}), 200
 
-        # using numpy.mean() in case the db gets too large for statistics.mean()
-        return jsonify({'payload': mean(ints)}), 200
-        # return jsonify({'payload': ledger.get_avg('ints')}), 200
+        if payload == 'float':
+            return jsonify({'payload': ledger.get_avg('floats')}), 200
 
-    if payload == 'float':
-        collection = db.floats.find()
-        floats = [item['value'] for item in collection]
+    else:
+        if payload == 'int':
+            collection = db.ints.find()
+            ints = [item['value'] for item in collection]
 
-        # using numpy.mean() in case the db gets too large for statistics.mean()
-        return jsonify({'payload': mean(floats)}), 200
-        # return jsonify({'payload': ledger.get_avg('floats')}), 200
+            # using numpy.mean() in case the db gets too large for statistics.mean()
+            return jsonify({'payload': mean(ints)}), 200
+
+        if payload == 'float':
+            collection = db.floats.find()
+            floats = [item['value'] for item in collection]
+
+            return jsonify({'payload': mean(floats)}), 200
 
 
 if __name__ == '__main__':
