d37ddf7b40a8fb568d08611198285c9875c7b15d
pal szentgyorgyi <pszentg@gmail.com>
2021-08-27 09:52:11 +0200

    introduced caching to eliminate concurrency. replaced ledger dicts with lists to increase scalability. query methods now are GET instead of POST. TODO: response messages

--- app/send.py
+++ app/send.py
@@ -14,19 +14,13 @@ data = {"payload": "mystring"}
 data_json = json.dumps(data)
 requests.post("http://127.0.0.1:5000", json=data_json, headers={'Content-type': 'application/json'})
 
-data = {"payload": 1}
-data_json = json.dumps(data)
-r = requests.post("http://127.0.0.1:5000/count", json=data_json, headers={'Content-type': 'application/json'})
+r = requests.get("http://127.0.0.1:5000/count/1")
 print(r.content)
 
-data = {"payload": 1.0}
-data_json = json.dumps(data)
-r = requests.post("http://127.0.0.1:5000/count", json=data_json, headers={'Content-type': 'application/json'})
+r = requests.get("http://127.0.0.1:5000/count/1.0")
 print(r.content)
 
-data = {"payload": "mystring"}
-data_json = json.dumps(data)
-r = requests.post("http://127.0.0.1:5000/count", json=data_json, headers={'Content-type': 'application/json'})
+r = requests.get("http://127.0.0.1:5000/count/mystring")
 print(r.content)
 
 # this is just for testing purposes
@@ -40,9 +34,7 @@ for i in range(5):
     data_json = json.dumps(data)
     requests.post("http://127.0.0.1:5000", json=data_json, headers={'Content-type': 'application/json'})
 
-data = {"payload": 'int'}
-data_json = json.dumps(data)
-r = requests.post("http://127.0.0.1:5000/avg", json=data_json, headers={'Content-type': 'application/json'})
+r = requests.get("http://127.0.0.1:5000/avg/ints")
 print(r.content)
 
 for i in range(5):
@@ -50,9 +42,8 @@ for i in range(5):
     data_json = json.dumps(data)
     requests.post("http://127.0.0.1:5000", json=data_json, headers={'Content-type': 'application/json'})
 
-data = {"payload": 'float'}
-data_json = json.dumps(data)
-r = requests.post("http://127.0.0.1:5000/avg", json=data_json, headers={'Content-type': 'application/json'})
+
+r = requests.get("http://127.0.0.1:5000/avg/floats")
 print(r.content)
 
 
--- app/server.py
+++ app/server.py
@@ -2,7 +2,14 @@ import json
 from logging.config import dictConfig
 from flask_pymongo import PyMongo
 from flask import Flask, request, jsonify
+from flask_caching import Cache
 import argparse
+import re
+
+parser = argparse.ArgumentParser(description='Process some incoming HTTP requests.')
+parser.add_argument('--ledger', '-l', help='Switches the server to store the data locally instead of a remote DB',
+                    action='store_true')
+args = parser.parse_args()
 
 dictConfig({
     'version': 1,
@@ -20,15 +27,21 @@ dictConfig({
     }
 })
 
-parser = argparse.ArgumentParser(description='Process some incoming HTTP requests.')
-parser.add_argument('--ledger', '-l', help='Switches the server to store the data locally instead of a remote DB',
-                    action='store_true')
-args = parser.parse_args()
+config = {
+    "DEBUG": True,
+    "CACHE_TYPE": "RedisCache",
+    "CACHE_DEFAULT_TIMEOUT": 300,
+    "MONGO_URI":
+        "mongodb+srv://user:HcSrNasKdaBBwJ0z@cluster0.wyb5l.mongodb.net/minimal_webapi?retryWrites=true&w=majority"
+}
+
 app = Flask(__name__)
-app.config["MONGO_URI"] = \
-    "mongodb+srv://user:HcSrNasKdaBBwJ0z@cluster0.wyb5l.mongodb.net/minimal_webapi?retryWrites=true&w=majority"
+app.config.from_mapping(config)
+
 mongo = PyMongo(app)
 db = mongo.db
+
+cache = Cache(app)
 
 if args.ledger:
     app.logger.info('starting server with ledger instead of DB')
@@ -42,8 +55,10 @@ def receive():
     data = json.loads(content)
     payload = data['payload']
     if args.ledger:
-        return_code = ledger.insert(payload)
-        if return_code == 501:
+        try:
+            ledger.insert(payload)
+            app.logger.info(f'ledgers are after insert:\n{ledger.ints}\n{ledger.floats}\n{ledger.strings}')
+        except TypeError:
             return jsonify('Unknown data type', success=False), 501
 
     else:
@@ -61,58 +76,69 @@ def receive():
     return jsonify(success=True)
 
 
-@app.route('/count', methods=["POST"])
-def query_count():
-    content = request.get_json()
-    data = json.loads(content)
-    payload = data['payload']
+@app.route('/count/<count_type>', methods=["GET"])
+@cache.cached(10, "ledger")
+def query_count(count_type):
+    int_regex = r"\d+(?=\.)"
+    float_regex = r"\d+\.\d+"
+
+    if re.match(int_regex, count_type):
+        query_type = 1
+    elif re.match(float_regex, count_type):
+        query_type = 1.0
+    else:
+        query_type = "string"
+
     if args.ledger:
-        app.logger.info(f'{payload} was submitted {ledger.get_count(payload)} according to the ledger')
-        return jsonify({'payload': ledger.get_count(payload)}), 200
+        app.logger.info(f'ledgers are before query:\n{ledger.ints}\n{ledger.floats}\n{ledger.strings}')
+        app.logger.info(f'{query_type} was submitted {ledger.get_count(count_type)} according to the ledger')
+        return jsonify({'payload': ledger.get_count(query_type)}), 200
 
     else:
-        query = {'value': payload}
-        if type(payload) is int:
+        query = {'value': query_type}
+        results = None
+        if type(query_type) is int:
             results = db.ints.count_documents(query)
-            app.logger.info(results)
-            app.logger.info(f'{payload} was submitted {results} times since the DB was initialized last.')
-            return jsonify({'payload': results}), 200
 
-        if type(payload) is float:
+        elif type(query_type) is float:
             results = db.floats.count_documents(query)
-            app.logger.info(results)
-            app.logger.info(f'{payload} was submitted {results} times since the DB was initialized last.')
-            return jsonify({'payload': results}), 200
 
-        if type(payload) is str:
+        elif type(query_type) is str:
             results = db.strings.count_documents(query)
+
+        if results:
             app.logger.info(results)
-            app.logger.info(f'{payload} was submitted {results} times since the DB was initialized last.')
+            app.logger.info(f'{query_type} was submitted {results} times since the DB was initialized last.')
             return jsonify({'payload': results}), 200
+        else:
+            return jsonify(success=False), 500
 
 
-@app.route('/avg', methods=["POST"])
-def query_avg():
-    content = request.get_json()
-    data = json.loads(content)
-    payload = data['payload']
+@app.route('/avg/<avg_type>', methods=["GET"])
+@cache.cached(10, "ledger")
+def query_avg(avg_type):
+
     if args.ledger:
-        if payload == 'int':
+        if avg_type == 'ints':
             return jsonify({'payload': ledger.get_avg('ints')}), 200
 
-        if payload == 'float':
+        if avg_type == 'floats':
             return jsonify({'payload': ledger.get_avg('floats')}), 200
+        return jsonify(succes=False), 500
 
     else:
         pipeline = [
             {"$group": {"_id": "1", "results": {"$avg": "$value"}}},
         ]
 
-        if payload == 'int':
+        if avg_type == 'ints':
             return jsonify({'payload': list(db.floats.aggregate(pipeline))[0]["results"]}), 200
 
-        if payload == 'float':
+        if avg_type == 'floats':
             return jsonify({'payload': list(db.floats.aggregate(pipeline))[0]["results"]}), 200
+
+        else:
+            return jsonify(success=False), 500
 
 
 if __name__ == '__main__':
